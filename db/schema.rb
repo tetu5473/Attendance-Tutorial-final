0a93c15744cd64883e22bdac12f1a7e85a3c1f7a
tetuya ohta <tetu5473@gmail.com>
2022-11-14 12:30:39 +0000

    Finish editing-users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221028214315) do
+ActiveRecord::Schema.define(version: 20221114112040) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20221028214315) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
